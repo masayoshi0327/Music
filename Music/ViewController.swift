24be55d76a09ee530fefb6d55c8e7602ccf6d3aa
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-17 17:49:53 +0900

    音楽とボタンの追加

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let music = Bundle.main.bundleURL.appendingPathComponent("A New Orleans Crawfish Boil - Unicorn Heads.mp3")
 
 
 }
